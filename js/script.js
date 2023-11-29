121877c889e73f132eb17629d748758d43aa9e4a
olangomark <43403873+olangomark@users.noreply.github.com>
2023-11-29 21:18:48 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 18, 2022 18:00:00").getTime();
+var countDownDate = new Date("December 7, 2023 15:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
